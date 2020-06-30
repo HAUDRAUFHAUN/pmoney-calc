74bcce18d32f38d1557dfc0b18f0618b07464737
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-06-30 18:17:03 +0200

    add new payment page to backend and start with creating it

--- pmoneycalc/pmoneytracker/models.py
+++ pmoneycalc/pmoneytracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Payment(models.Model):
+    """Model definition for Payment."""
+    payment = models.FloatField()
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Payment."""
+
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+
+    def __str__(self):
+        """Unicode representation of Payment."""
+        pass
--- pmoneycalc/pmoneytracker/views.py
+++ pmoneycalc/pmoneytracker/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def newpayment(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'pmoneytracker/newpayment.html')
